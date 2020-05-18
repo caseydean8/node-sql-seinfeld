160a8a20b366408aaf3e4defbdac6697aabe0a5c
Casey Carroll <caseydean8@gmail.com>
2020-05-18 09:41:24 -0400

    GET character by attitude route created

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-// * Create a Node Application with Express and MySQL with three Express routes.
-
-//     * Create a `/cast` route that will display all the actors and their data ordered by their id's.
-
-//     * Create a `/attitude-chart/:att` route that will display all the actors for a specific type of attitude.
 const express = require("express");
 const mysql = require("mysql");
 
@@ -15,60 +10,77 @@ const connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: "fatbeats",
-  database: "seinfeld_db"
+  database: "seinfeld_db",
 });
 
-connection.connect(err => {
+connection.connect((err) => {
   if (err) {
     console.error(`error connecting: ${err.stack}`);
     return;
   }
   console.log(`connected as id ${connection.threadId}`);
-})
+});
+
+let html = "";
 
 // Routes
-app.get("/", function(req, res) {
-
-  // If the main route is hit, then we initiate a SQL query to grab all records.
-  // All of the resulting records are stored in the variable "result."
+app.get("/", function (req, res) {
+  //Initiate sql query
   connection.query("SELECT * FROM characters", (err, result) => {
     if (err) throw err;
-    // We then begin building out HTML elements for the page.
-    var html = "<h1> Seinfeld Characters </h1>";
-
-    // Here we begin an unordered list.
+    // Build html
+    html = "<h1> Seinfeld Characters </h1>";
     html += "<ul>";
 
-    // We then use the retrieved records from the database to populate our HTML file.
+    // Use result to populate html with data
     for (var i = 0; i < result.length; i++) {
       html += "<li><p> ID: " + result[i].id + "</p>";
       html += "<p>" + result[i].name + " </p></li>";
     }
 
-    // We close our unordered list.
     html += "</ul>";
 
-    // Finally we send the user the HTML file we dynamically created.
+    // Send html to user
     res.send(html);
   });
 });
 
 // "coolness" route
 app.get("/coolness", (req, res) => {
-  connection.query("select * from characters order by coolness desc", (err, result) => {
-    if (err) throw err;
+  connection.query(
+    "select * from characters order by coolness desc",
+    (err, result) => {
+      if (err) throw err;
 
-    let html = "<h1>Characters</h1>";
-    html += "<ul>";
+      html = "<h1>Characters</h1>";
+      html += "<ul>";
 
-    result.forEach(character => {
-      html += `<li><p>${character.name}</p>`;
-      html += `<p>coolness points: ${character.coolness}</p></li>`;
-    });
-    
-    html += "</ul>";
-    res.send(html);
-  })
-})
+      result.forEach((character) => {
+        html += `<li><p>${character.name}</p>`;
+        html += `<p>coolness points: ${character.coolness}</p></li>`;
+      });
+
+      html += "</ul>";
+      res.send(html);
+    }
+  );
+});
+
+//Attitude route displays character with specific attitude
+app.get("/attitude/:att", (req, res) => {
+  let attitude = req.params.att;
+  connection.query(
+    `select * from characters where attitude = "${attitude}"`,
+    (err, result) => {
+      if (err) throw err;
+      let html = `<h1>Who's the ${attitude} character?</h1>`;
+      result.forEach((character) => {
+        html += `<li><p>${character.name}</p></li>`;
+      });
+      html += "</ul>";
+      res.send(html);
+    }
+  );
+});
 
 app.listen(PORT, () => console.log(`Listening http://localhost:${PORT}`));
